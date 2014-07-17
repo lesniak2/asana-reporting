16897860b107684b64540766b526abde79f7c630
lesniak2 <tomlesniak@hotmail.com>
2014-07-17 14:24:52 -0500

    Added story and project parsing capabilities

---
+++ AsanaCrescent/AsanaCrescent/Parser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using AsanaNet;
+
+namespace AsanaCrescent
+{
+    static class Parser
+    {
+        public static string ParseStory(AsanaStory story, string s)
+        {    
+            return story.Text.LastIndexOf(s) != -1 ? story.CreatedAt.ToString() : null;
+        }
+
+        public static string[] ParseSubProjects(ArrayList tasks)
+        {
+            ArrayList subprojects = new ArrayList();
+            foreach(var o in tasks)
+            {
+                AsanaTask task = o as AsanaTask;
+                if(task.Name.EndsWith(':')) 
+                {
+                    subprojects.Add(task.Name);
+                }
+            }
+            return (string[]) subprojects.ToArray();
+        }
+    }
+}
