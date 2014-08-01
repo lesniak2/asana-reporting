48d7b12b9f8fd7d303d358acd8fd9509ce73e50b
twill14 <twill14@gmail.com>
2014-08-01 12:56:28 -0500

    Imported the Asana Crescent logo

--- AsanaCrescent/AsanaCrescent/Properties/Resources.Designer.cs
+++ AsanaCrescent/AsanaCrescent/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace WindowsFormsApplication1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap asanacrescent2 {
+            get {
+                object obj = ResourceManager.GetObject("asanacrescent2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap leapfrog {
             get {
                 object obj = ResourceManager.GetObject("leapfrog", resourceCulture);
