686d6bb7eebbbd4deb5ace4070d826ec1b71ef17
lesniak2 <tomlesniak@hotmail.com>
2014-08-05 09:55:04 -0500

    Added basic custom checkbox class

---
+++ AsanaCrescent/AsanaCrescent/AsanaCheckbox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace AsanaCrescent
+{
+    class AsanaCheckbox : CheckBox
+    {
+        public AsanaCheckbox() { }
+        protected override void OnPaint(PaintEventArgs e) 
+        {
+
+
+            Graphics g = e.Graphics;
+
+            base.OnPaint(e);
+            if (this.Checked)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Blue), new Rectangle(0, 0, 16, 16));
+            }
+            else
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Red), new Rectangle(0, 0, 16, 16));
+            }
+
+
+       }
+    }
+}
