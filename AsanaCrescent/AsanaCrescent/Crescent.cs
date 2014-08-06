6686892c257ff2df088d44809c8c814c0860f52c
lesniak2 <tomlesniak@hotmail.com>
2014-08-06 12:45:04 -0500

    Removed unsused "using" statements

--- AsanaCrescent/AsanaCrescent/Crescent.cs
+++ AsanaCrescent/AsanaCrescent/Crescent.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Collections;
-using AsanaNet;
 using System.Windows.Forms;
 using System.Threading;
 using System.ComponentModel;
+using AsanaNet;
 namespace AsanaCrescent
 {
     enum AsanaManager
